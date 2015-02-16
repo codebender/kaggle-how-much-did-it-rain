a3e389921bb526ba1f7c15d0b29c982784997201
Matthew Bender <benderm@gmail.com>
2015-02-16 14:53:15 -0700

    all 1s, :umbrella: score = 0.01017651

---
+++ all_ones.R
@@ -0,0 +1,11 @@
+test <- read.csv("Data/test_2014.csv", stringsAsFactors=FALSE)
+
+
+mat <- matrix(1, ncol=70)
+
+submit <- data.frame(Id = test$Id, mat)
+submit <- subset(submit, select=c(1,71,2:70))
+names(submit) <- gsub("X", "Predicted", names(submit))
+colnames(submit)[2] <- "Predicted0"
+
+write.csv(submit, file = "Output/all_ones.csv", row.names = FALSE)
